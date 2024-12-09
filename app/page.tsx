f1de072933b4f88ac5e806e78cfeb08a88365d0d
twesigetrejan <trejantwesige19@gmail.com>
2024-12-09 10:49:18 +0300

    button woes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Christian  Freedom Church Website 
+      Christian Freedom Church website
+      <button className="button-cfc" role="button">Join us</button>
+
+
     </div>
   );
 }
