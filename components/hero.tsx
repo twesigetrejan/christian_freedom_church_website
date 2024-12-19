171e68243fcaeb2327364d0adc41621334679da8
twesigetrejan <trejantwesige19@gmail.com>
2024-12-19 19:07:50 +0300

    hero section done

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="px-10">
-      <div className="items-center flex justify-center pt-[52px] pb-[15px]">
+      <div className="items-center flex justify-center pt-[52px] pb-[60px]">
         <Image
           src={"/Images/hero-image.png"}
           alt={"hero image"}
@@ -13,16 +13,16 @@ const Hero = () => {
           className="object-cover "
         />
       </div>
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start pb-[52px]">
         <h1 className="text-dark font-bold text-[64px] ">
           Become part of something great
         </h1>
 
         <div className="flex justify-center items-center gap-[37px]">
-          <h1 className="text-dark text-[64px] font-bold">
+          <h1 className="text-dark text-[64px] font-bold font-Montserrat">
             today, cfc welcomes you
           </h1>
-          <button className="btn flex items-center">
+          <button className="btn flex items-center font-Montserrat">
             Get started
             <ArrowRight className="w-4 h-4 ml-2" />
           </button>
