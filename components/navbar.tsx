d860aacb395cd486384560fa126e7935aab258b2
twesigetrejan <trejantwesige19@gmail.com>
2024-12-20 18:34:00 +0300

    navbar woes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -26,30 +26,59 @@ const Navbar = () => {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
-          isScrolled ? "py-2 bg-opacity-10" : "py-4 bg-opacity-50"
-        } bg-white backdrop-blur-md`}
+        className={`fixed top-0 left-0 w-full z-50 bg-white transition-all duration-1000 ${
+          isScrolled ? "py-1 px-5" : "py-4 px-10"
+        } bg-opacity-90 backdrop-blur-md`}
       >
-        <div className="flex justify-between items-center px-10">
-          <div className="flex">
+        <div
+          className={`flex items-center transition-all duration-1000 ${
+            isScrolled ? "gap-3 justify-center" : "gap-[44px] justify-between"
+          }`}
+        >
+          {/* Logo */}
+          <div
+            className={`transition-all duration-1000 ${
+              isScrolled ? "w-10 mr-[420px]" : "w-16"
+            }`}
+          >
             <Logo />
           </div>
-          <div className="flex gap-[44px] justify-center items-center font-sans">
+
+          {/* Navigation Links */}
+          <div
+            className={`flex items-center transition-all duration-1000 ${
+              isScrolled ? "text-[15px] gap-2" : "text-base gap-[44px]"
+            }`}
+          >
             {navlinks &&
               navlinks.map((navlink) => (
-                <Link className="" key={navlink.id} href={navlink.route}>
-                  <p className="">{navlink.title}</p>
+                <Link
+                  key={navlink.id}
+                  href={navlink.route}
+                  className="transition-all duration-1000"
+                >
+                  <p className={isScrolled ? "px-2" : "px-3"}>
+                    {navlink.title}
+                  </p>
                 </Link>
               ))}
           </div>
-          <div className="rounded-[14px] bg-primary flex px-5 py-[6px] justify-center text-white hover:text-primary hover:bg-white">
-            <button className="">Donate</button>
+
+          {/* Donate Button */}
+          <div
+            className={`rounded transition-all duration-1000 ${
+              isScrolled
+                ? "bg-primary px-2 py-1 text-xs ml-[420px]"
+                : "bg-primary px-5 py-[6px] text-base ml-10"
+            } text-white hover:text-primary hover:bg-white`}
+          >
+            <button>Donate</button>
           </div>
         </div>
       </div>
 
       {/* Spacer to prevent content overlap */}
-      <div className={isScrolled ? "h-[48px]" : "h-[72px]"} />
+      <div className={isScrolled ? "h-[30px]" : "h-[72px]"} />
     </>
   );
 };
