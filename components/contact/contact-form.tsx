ea48ef22cad43022c10720c20d131c609bc4d740
twesigetrejan <trejantwesige19@gmail.com>
2024-12-27 22:13:50 +0300

    component update

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, ChangeEvent, FormEvent } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
