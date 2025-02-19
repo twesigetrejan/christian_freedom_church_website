b22bb11571ef59e48bd3094d12dd90fad0a1af6b
twesigetrejan <trejantwesige19@gmail.com>
2025-02-19 10:31:57 +0300

    styles( about) - styles changes for the about page

--- components/about/mission-vision.tsx
+++ components/about/mission-vision.tsx
@@ -19,6 +19,7 @@ export const MissionVision = () => {
             width={350}
             height={330}
             alt="Mission Image"
+            className="rounded-2xl"
           />
         </div>
       </div>
@@ -32,6 +33,7 @@ export const MissionVision = () => {
             width={350}
             height={330}
             alt="Mission Image"
+            className="rounded-2xl"
           />
         </div>
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -76,7 +76,7 @@ const Navbar = () => {
                 : "bg-primary px-5 text-base ml-4"
             } text-white hover:scale-110`}
           >
-            <button onClick={GiveAction}>Donate</button>
+            <button className="" onClick={GiveAction}>Donate</button>
           </div>
 
           <div className="lg:hidden mr-4">
